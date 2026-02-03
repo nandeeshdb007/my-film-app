f09ebd01cc5beff3471542940746e55a3d347eb7
nandeeshdb007 <markbotoffical@gmail.com>
2026-02-03 19:34:14 +0530

    feat: created-trending-now-movie-section

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import {  StyleSheet, ScrollView, View } from 'react-native'
+import {  StyleSheet, ScrollView, View, FlatList } from 'react-native'
 import COLORS from '../../constants/colors'
 import OverviewSection from './OverviewSection';
 import SectionHeader from './SectionHeader';
 import MovieCard from '../UI/MovieCard';
+import { movies } from '../../constants/mock-data';
 
 const Home = () => {
   return (
@@ -12,7 +13,14 @@ const Home = () => {
       <View style={{flex:1}}>
         <View style={{marginVertical: 20}} >
           <SectionHeader title='Trending now'/>
-        <MovieCard />
+          <FlatList
+          data={movies}
+          renderItem={({item})=>(
+
+            <MovieCard image={item.image} title={item.title} genre={item.genre} />
+          )}
+          horizontal
+          />
         </View>
 
       </View>
