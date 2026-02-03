1281087eaf588b92ad707d8d33a61b538a818c6e
nandeeshdb007 <markbotoffical@gmail.com>
2026-02-03 19:31:01 +0530

    feat: created-dummy-movie-data

---
+++ src/constants/mock-data.ts
@@ -0,0 +1,67 @@
+export const movies = [
+  {
+    image: require("../../assests/images/movie1.png"),
+    title: "The Sandan",
+    genre: "Action | Drama",
+  },
+  {
+    image: require("../../assests/images/movie2.png"),
+    title: "Shaka Ilembe",
+    genre: "Action | Drama",
+  },
+  {
+    image: require("../../assests/images/movie3.png"),
+    title: "Savage Kingdom",
+    genre: "Adventure | Wildlife",
+  },
+  {
+    image: require("../../assests/images/movie4.png"),
+    title: "Ghost Protocol",
+    genre: "Action | Thriller",
+  },
+  {
+    image: require("../../assests/images/movie5.png"),
+    title: "Royal Blood",
+    genre: "Drama | Mystery",
+  },
+  {
+    image: require("../../assests/images/movie6.png"),
+    title: "Echoes of War",
+    genre: "Historical | Action",
+  },
+  {
+    image: require("../../assests/images/movie7.png"),
+    title: "The Outpost",
+    genre: "War | Action",
+  },
+  {
+    image: require("../../assests/images/movie8.png"),
+    title: "The King's Path",
+    genre: "Drama | Biography",
+  },
+  {
+    image: require("../../assests/images/movie9.png"),
+    title: "Dark Uprising",
+    genre: "Sci-Fi | Action",
+  },
+  {
+    image: require("../../assests/images/movie10.png"),
+    title: "The Last Heir",
+    genre: "Fantasy | Drama",
+  },
+  {
+    image: require("../../assests/images/movie11.png"),
+    title: "Blood & Water",
+    genre: "Drama | Teen",
+  },
+  {
+    image: require("../../assests/images/movie12.png"),
+    title: "Code of Honor",
+    genre: "Action | Crime",
+  },
+  {
+    image: require("../../assests/images/movie13.png"),
+    title: "Echo Valley",
+    genre: "Mystery | Thriller",
+  },
+];
