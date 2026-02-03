17ba79bc293dcb081f7a9c2338e7bcb16186ea38
nandeeshdb007 <markbotoffical@gmail.com>
2026-02-03 12:58:58 +0530

    installed-bottom-tab-navigation-packages

--- App.tsx
+++ App.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import { View } from 'react-native'
+import { Text, View } from 'react-native'
 
 const App = () => {
   return (
     <View>
-      Hello word
+      <Text>Hello word</Text> 
     </View>
   )
 }
